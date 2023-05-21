bd33907304f0305d0234fb3c91068173235c64eb
Ilia Shevchenko <sheff.ilya2010@gmail.com>
2023-05-21 11:32:49 +0300

    Вторая программа_Погружение в программирование на JavaScript

--- projects.js
+++ projects.js
@@ -5,3 +5,21 @@ let timeInMinutes = 180;
 let jumps = (timeInMinutes/10)*3;
 let message = "За "  + timeInMinutes + " минут в соцсетях полагается " + jumps + " прыжков.";
 console.log(message);
+
+//Вторая программа.
+let temperature = 20;
+let isRaining = true;
+let minutes = 0;
+
+if (isRaining) {
+  minutes = 0;
+} else if  (temperature >= 10 && temperature < 15) {
+    minutes = 30;
+  } else if (temperature >= 15 && temperature < 25) {
+    minutes = 40;
+  } else if (temperature >= 25 && temperature <= 35) {
+    minutes = 20;
+  } else {
+    minutes = 0;
+  }
+console.log(minutes);
