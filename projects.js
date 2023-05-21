37f43a4303384dc25310b7fbc45c7fdcc42870c9
Ilia Shevchenko <sheff.ilya2010@gmail.com>
2023-05-21 11:31:36 +0300

    Первая программа_Погружение в программирование на JavaScript

---
+++ projects.js
@@ -0,0 +1,7 @@
+// Курс «Погружение в программирование на JavaScript»
+
+//Первая программа.
+let timeInMinutes = 180;
+let jumps = (timeInMinutes/10)*3;
+let message = "За "  + timeInMinutes + " минут в соцсетях полагается " + jumps + " прыжков.";
+console.log(message);
